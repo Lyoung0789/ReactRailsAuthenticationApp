90807314b93f0746e9b39254bef8d50384b2bdfc
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-30 21:22:03 -0400

    Adding the peristence of a user being logged in and creating an option for logging out

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Login from './auth/Login';
 import Registration from './auth/Registration'
+import axios from 'axios'
 
 export default class Home extends Component {
 
@@ -15,11 +16,23 @@ export default class Home extends Component {
         this.props.history.push("./dashboard")
     }
 
+    handleLogoutClick = () =>{
+        axios.delete("http://localhost:3001/logout", {withCredentials: true})
+        .then(response => {
+            console.log("Worked")
+        })
+        .catch(errors =>{
+            console.log("logout error", error)
+        })
+        this.props.handleLogout()
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
+                <button onClick={this.handleLogoutClick}>Logout</button>
                 <Registration handleSucessfulAuth={this.handleSucessfulAuth}/>
                 <Login handleSucessfulAuth={this.handleSucessfulAuth}/>
             </div>
--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom"
+import axios from 'axios'
 import Dashboard from './Dashboard';
 import Home from './Home';
 
@@ -21,13 +22,44 @@ export default class App extends Component {
 
   }
 
+  handleLogout = () =>{
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN",
+      user: {}
+    })
+  }
+
+  checkLoginStatus =() =>{
+    axios.get("http://localhost:3001/logged_in", {withCredentials: true}).then(response => {
+      console.log("Logged In?", response)
+      if(response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if(!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN"){
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => {
+      console.log("Login Error: ", error)
+    })
+  }
+
+  componentDidMount(){
+    this.checkLoginStatus()
+  }
+
   render() {
     return (
       <div className='app'>
         <BrowserRouter>
         <Switch>
           <Route exact path={"/"} render={props => (
-            <Home {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.loggedInStatus} />
+            <Home {...props} handleLogout={this.handleLogout} handleLogin={this.handleLogin} loggedInStatus={this.state.loggedInStatus} />
           )}/>
           <Route exact path={"/dashboard"} render={props => (
             <Dashboard {...props} loggedInStatus={this.state.loggedInStatus} />
